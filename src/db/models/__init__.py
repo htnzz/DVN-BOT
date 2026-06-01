a2f6216126a4a59621730911287723c53dadc4cd
htnzz <borntodie4@mail.ru>
2026-06-01 22:23:20 +1000

    Правки моделей Удалил лишний импорт

--- src/db/models/__init__.py
+++ src/db/models/__init__.py
@@ -2,7 +2,6 @@ from src.db.models.audit_log import AuditLog
 from src.db.models.comment import Comment
 from src.db.models.notification import Notification
 from src.db.models.object_ref import ObjectRef
-from src.db.models.photo_report import PhotoReport
 from src.db.models.status_change import StatusChange
 from src.db.models.user import User
 from src.db.models.base import Base
@@ -12,7 +11,6 @@ __all__ = (
     "ObjectRef",
     "AuditLog",
     "Comment",
-    "PhotoReport",
     "Notification",
     "StatusChange",
     "Base",
