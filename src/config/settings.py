918dadcdfd3d12b5fb215984a7651b12fba2aba9
htnzz <borntodie4@mail.ru>
2026-06-06 14:02:17 +1000

    Обновил settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     postgres_host: str = Field(alias="POSTGRES_HOST")
     postgres_port: int = Field(alias="POSTGRES_PORT")
     postgres_username: str = Field(alias="POSTGRES_USERNAME")
+    postgres_db: str = Field(alias="POSTGRES_DB")
     postgres_password: SecretStr = Field(alias="POSTGRES_PASSWORD")
 
     gis_api_base_url: str = Field(alias="GIS_API_BASE_URL")
@@ -25,7 +26,7 @@ class Settings(BaseSettings):
         password = self.postgres_password.get_secret_value()
         return (
             f"postgresql+asyncpg://{self.postgres_username}:{password}"
-            f"@{self.postgres_host}:{self.postgres_port}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
         )
 
 
